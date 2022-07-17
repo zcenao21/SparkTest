ade276f88da8f5127a74d08a31c590ff6c463262
will <zcenao21@gmail.com>
2022-07-18 00:14:04 +0800

    fix

--- spark-core/src/main/java/com/will/spark/core/AggregateByKeyTest.scala
+++ spark-core/src/main/java/com/will/spark/core/AggregateByKeyTest.scala
@@ -7,7 +7,7 @@ object AggregateByKeyTest {
         val conf = new SparkConf().setMaster("local[*]").setAppName("Spark Word Count")
         val sc = new SparkContext(conf)
         val rddIn = sc.parallelize(List(("a",1),("a",2),("a",3),("b",4)),3)
-        val groupRDD = rddIn.aggregateByKey((0,0))((t,v)=>(t._1+v,t._2+1),(t1,t2)=>(t1._1+t2._1, t1._2+t2._2))
+        rddIn.aggregateByKey((0,0))((t,v)=>(t._1+v,t._2+1),(t1,t2)=>(t1._1+t2._1, t1._2+t2._2))
             .map(line=>(line._1, line._2._1/line._2._2))
             .collect()
             .foreach(println)
